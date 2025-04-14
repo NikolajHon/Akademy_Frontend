e3c59ce8b8ca9aa4dca8469eeaa4233660584fdf
Nikolaj <goncharenkokolya3@gmail.com>
2025-04-14 11:06:35 +0200

    Moved the data for the keycloak to the environment file

--- src/app/core/config/authCodeFlowConfig.config.ts
+++ src/app/core/config/authCodeFlowConfig.config.ts
@@ -1,10 +1,11 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
+import {environment} from '../../environments/environment';
 
 export const authCodeFlowConfig: AuthConfig = {
-  issuer: 'http://localhost:8081/realms/akademia_project',
-  redirectUri: window.location.origin,
-  clientId: 'discussion-backend',
-  responseType: 'code',
-  scope: 'openid profile email',
-  showDebugInformation: true
+  issuer: environment.auth.issuer,
+  redirectUri: environment.auth.redirectUri,
+  clientId: environment.auth.clientId,
+  responseType: environment.auth.responseType,
+  scope: environment.auth.scope,
+  showDebugInformation: environment.auth.showDebugInformation,
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,11 @@
 export const environment = {
   production: false,
-  appUrl: 'http://localhost:4200',
-  keyCloakUrl: 'http://localhost:8081/auth',
-  realm: 'akademia_project',
-  clientId: 'discussion-backend',
+  auth: {
+    issuer: 'http://localhost:8081/realms/akademia_project',
+    redirectUri: window.location.origin,
+    clientId: 'discussion-backend',
+    responseType: 'code',
+    scope: 'openid profile email',
+    showDebugInformation: true,
+  },
 };
