7454ba85ef4b21c9856331c49d112751c2e298ef
Nikolaj <goncharenkokolya3@gmail.com>
2025-05-26 15:52:51 +0200

    realized the limitation of courses for students

--- src/app/features/courses/pages/course-list-page/course-list-page.component.ts
+++ src/app/features/courses/pages/course-list-page/course-list-page.component.ts
@@ -1,16 +1,15 @@
-import {
-  Component, OnInit,
-  WritableSignal, signal, computed, inject
-} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import {Component, computed, inject, OnInit, signal, WritableSignal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
 
-import { Course } from '../../models/course.model';
-import { CourseService } from '../../services/course.service';
-
-import { CourseListComponent } from
-    '../../components/course-list/course-list.component';
+import {Course} from '../../models/course.model';
+import {CourseService} from '../../services/course.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import {CourseListComponent} from '../../components/course-list/course-list.component';
+import {UserModel} from '../../../../core/models/user-model';
+import {UserService} from '../../../../core/services/user.service';
+import {UserRole} from '../../../../core/models/user-role-enum';
 
 @Component({
   selector: 'app-course-list-page',
@@ -22,15 +21,16 @@ import { CourseListComponent } from
 export class CourseListPageComponent implements OnInit {
   private courseService = inject(CourseService);
   private router = inject(Router);
-
+  courses: Course[] = [];
+  filtered: Course[] = [];
   // Сигналы для списка и поиска
   private coursesSig: WritableSignal<Course[]> = signal([]);
   private searchQuery: WritableSignal<string> = signal('');
 
-  // Сигналы для модалки
   isModalOpen: WritableSignal<boolean> = signal(false);
   newCourseName = '';
   newCourseDescription = '';
+  currentUser?: UserModel;
 
   filteredCourses = computed(() => {
     const q = this.searchQuery().toLowerCase().trim();
@@ -41,8 +41,11 @@ export class CourseListPageComponent implements OnInit {
         c.description.toLowerCase().includes(q)
       );
   });
-
+  constructor(
+    private userService: UserService
+  ) {}
   ngOnInit() {
+    this.currentUser = this.userService.getUserSignal()!();
     this.loadCourses();
   }
 
@@ -56,7 +59,26 @@ export class CourseListPageComponent implements OnInit {
   }
 
   openCourse(id: number): void {
-    this.router.navigate(['/course-page', id]);
+    if (this.currentUser?.role === UserRole.TEACHER) {
+      this.router.navigate(['/course-page', id]);
+    } else {
+      this.userService.getUserCourseRating(this.currentUser!.id, id)
+        .subscribe({
+          next: () => {
+            this.router.navigate(['/course-page', id]);
+          },
+          error: (err: HttpErrorResponse) => {
+            if (err.status === 404) {
+              window.alert(
+                'У вас нет доступа к этому курсу. Обратитесь к вашему учителю для предоставления доступа.'
+              );
+            } else {
+              console.error(err);
+              window.alert('Не удалось проверить доступ к курсу. Попробуйте ещё раз.');
+            }
+          }
+        });
+    }
   }
 
   // Открыть/закрыть модалку
